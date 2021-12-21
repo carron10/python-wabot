9a4e5158bad723c6ebd7748d1f0126f9bd8f6f5a
carron10 <carronmuleya10@gmail.com>
2021-12-22 00:18:11 +0200

    l

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from flask import Flask, redirect, url_for, request
 from users import users
 from message import message
 from user import user
+import logging
 
 account_sid  ="AC7dfb1e683991fbc0c74dce6a58230862"
 auth_token ="cc79eedc5c00cfd89fc960aa25db257e"
@@ -16,17 +17,23 @@ def send_msg(to,frm,body):
         print(message.sid)
         
 app = Flask(__name__)
+logging.basicConfig(filename='/opt/pybot.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+ 
 @app.route('/')
 def index():
+        
         return "Helo your app is running"
 @app.route('/bot/test/',methods=['POST'])
 def bot():
         msg=message(request.form)
+        app.logger.info('Info level log')
+        app.logger.warning('Warning level log')
         send=""
-        if msg.get_frm in users:
+        if msg.get_frm() in users:
                 user=users.get_users()[msg.get_frm()]
+                send_msg(msg.get_frm(),request.form['To'],"Testing messages")
         else:
-                users.get_users()[msg.get_frm()]=user(msg.get_frm,cart(),msg)
+                users.get_users()[msg.get_frm()]=user(msg.get_frm(),cart(),msg)
                 send="Thank you for sending your message"
                 send_msg(msg.get_frm(),request.form['To'],send)
                 
