d0aa60b61f1cc2d108b740efd98e31b911ba24c5
carron10 <carronmuleya10@gmail.com>
2021-12-22 12:06:31 +0200

    m

--- bot.py
+++ bot.py
@@ -3,7 +3,21 @@ from flask import Flask, redirect, url_for, request, render_template, make_respo
 account_sid = "AC7dfb1e683991fbc0c74dce6a58230862"
 auth_token = "cc79eedc5c00cfd89fc960aa25db257e"
 app = Flask(__name__)
-@app.route('/bot/test', methods=['POST'],strict_slashes=False)
+@app.route('/')
+def index():
+        return render_template("index.html")
+
+@app.route('/about')
+def about():
+        return render_template("about.html")
+@app.route('/contact')
+def contact():
+        return render_template("contact.html")
+@app.route('/products')
+def products():
+        return render_template("products.html")
+
+@app.route('/bot/test', methods=['POST'])
 def bot():
         return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Message><Body>Hello</Body></Message></Response>"
 if __name__ == '__main__':
