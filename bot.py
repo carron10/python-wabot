714112394aae21f10bef34eba3be1471e8ca4087
carron10 <carronmuleya10@gmail.com>
2021-12-22 12:23:45 +0200

    m

--- bot.py
+++ bot.py
@@ -9,6 +9,21 @@ logging.basicConfig(filename="/opt/pybot.log",level=logging.DEBUG,format=f'%(asc
 def send_msg(to, frm, body):
         message = client.messages.create(body=body, from_=frm, to=to)
         print(message.sid)
+@app.route('/bot/test', methods=['POST'])
+def bot():
+        global user,cart
+        msg=message(request.form)
+        send=""
+        if msg.get_frm() in users.get_users().keys():
+                send_msg(msg.get_frm(),request.form['To'],"Testing messages")
+        else:
+                cart=cart(msg.get_frm())
+                users.get_users()[msg.get_frm()]=user(msg.get_frm(),cart,msg)
+                send="Thank you for sending your message"
+                send_msg(msg.get_frm(),request.form['To'],send)
+        user=users.get_users()[msg.get_frm()]
+        user.set_prv_msg(msg)
+        user.set_last_msg_sent(send)
 @app.route('/')
 def index():
         return render_template("index.html")
@@ -22,25 +37,5 @@ def contact():
 @app.route('/products')
 def products():
         return render_template("products.html")
-
-@app.route('/bot/test', methods=['POST'])
-def bot():
-        global user,cart
-        msg=message(request.form)
-        app.logger.info('Info level log')
-        app.logger.warning('Warning level log')
-        send=""
-        if msg.get_frm() in users.get_users().keys():
-                send_msg(msg.get_frm(),request.form['To'],"Testing messages")
-        else:
-                
-                cart=cart(msg.get_frm())
-                users.get_users()[msg.get_frm()]=user(msg.get_frm(),cart,msg)
-                send="Thank you for sending your message"
-                send_msg(msg.get_frm(),request.form['To'],send)
-                
-        user=users.get_users()[msg.get_frm()]
-        user.set_prv_msg(msg)
-        user.set_last_msg_sent(send)
 if __name__ == '__main__':
    app.run(host='0.0.0.0',port=443,ssl_context=('cert.pem', 'key.pem'))
