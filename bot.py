76c753a06018d993c6891850417c5f63867e5b5c
carron10 <carronmuleya10@gmail.com>
2021-12-22 12:38:56 +0200

    m

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import os
-from flask import Flask, redirect, url_for, request, render_template, make_response
 import logging
+from flask import Flask, redirect, url_for, request, render_template, make_response
+from users import users
+from message import message
+from user import user
+from cart import cart
+
 account_sid = "AC7dfb1e683991fbc0c74dce6a58230862"
 auth_token = "cc79eedc5c00cfd89fc960aa25db257e"
 app = Flask(__name__)
@@ -8,7 +13,6 @@ logging.basicConfig(filename="/opt/pybot.log",level=logging.DEBUG,format=f'%(asc
 @app.route('/')
 def index():
         return render_template("index.html")
-
 @app.route('/about')
 def about():
         return render_template("about.html")
@@ -18,9 +22,23 @@ def contact():
 @app.route('/products')
 def products():
         return render_template("products.html")
+
+def send_msg(body):
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Message><Body>{0}</Body></Message></Response>".format{body}
 @app.route('/bot/test', methods=['POST'])
 def bot():
-        
-        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Message><Body>Hello</Body></Message></Response>"
+        global user,cart
+        msg=message(request.form)
+        send=""
+        if msg.get_frm() in users.get_users().keys():
+                send="testing...."
+        else:
+                cart=cart(msg.get_frm())
+                users.get_users()[msg.get_frm()]=user(msg.get_frm(),cart,msg)
+                send="Thank you for sending your message"
+        user=users.get_users()[msg.get_frm()]
+        user.set_prv_msg(msg)
+        user.set_last_msg_sent(send)
+        return send_msg(send)
 if __name__ == '__main__':
    app.run(host='0.0.0.0',port=443,ssl_context=('cert.pem', 'key.pem'))
