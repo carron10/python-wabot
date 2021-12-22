d8a7862561d4f78291c0093091d0eb093eab7dbc
carron10 <carronmuleya10@gmail.com>
2021-12-22 06:58:33 +0200

    m

--- bot.py
+++ bot.py
@@ -55,4 +55,4 @@ def bot():
         
         return "hhjhbh"
 if __name__ == '__main__':
-   app.run(host='0.0.0.0',port=8080,ssl_context=('cert.pem', 'key.pem'))
+   app.run(host='0.0.0.0',port=8080,ssl_context=('/opt/cert.pem', '/opt/key.pem'))
