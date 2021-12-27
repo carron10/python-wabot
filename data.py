718944ff34974a6fb76a674bb6f054fa788c001f
carron10 <carronmuleya10@gmail.com>
2021-12-27 22:55:08 +0200

    MM

--- data.py
+++ data.py
@@ -1,12 +1,12 @@
 data={
     "welcome":"Thank you for contacting us what name should we call you?",
     "home":'''Thank you {0} ,This platform is there to help you view our products and service. Please enter one\
- of  the options below to proceed <br>
+ of  the options below to proceed
  1. List Products
  2. View Cart
  3. Show Order''',
  "home_alt":'''Please enter one\
- of  the options below to proceed <br>
+ of  the options below to proceed 
  1. List Products
  2. View Cart
  3. Show Order'''
