300b50c54a11c8f53d4478ef2feabb2c33d46a69
carron10 <77936477+carron10@users.noreply.github.com>
2022-12-07 13:05:33 +0200

    add db credintials

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 class database:
 
     def __init__(self,db,user,pswd):
-        self.db=db
-        self.user=user
-        self.pswd=pswd
-    def get_connection():
-        #To return the connection to the database
+        self.conn = psycopg2.connect(
+        host=os.getenv("postgres_host"),
+        database=os.getenv("postgres_db"),
+        user=os.getenv("postgres_user"),
+        password=os.getenv("postgres_pswd"),
+        port=os.getenv("postgres_port")
+        )
+    def get_connection(self):
+        return self.conn
