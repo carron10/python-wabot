4df6d93f96d43e87b7dbf886c125106f6b4fb60b
carron10 <77936477+carron10@users.noreply.github.com>
2022-12-07 17:33:35 +0200

    M

--- database.py
+++ database.py
@@ -8,8 +8,7 @@ class database:
         host=os.getenv("postgres_host"),
         database=os.getenv("postgres_db"),
         user=os.getenv("postgres_user"),
-        password=os.getenv("postgres_pswd"),
-        port=os.getenv("postgres_port")
+        password=os.getenv("postgres_pswd")
         )
         cur=self.conn.cursor()
         cur.execute(''''create table if not exists my_data(
